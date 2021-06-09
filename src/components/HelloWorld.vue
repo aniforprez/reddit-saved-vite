5a97a715b742e28be116ce5ea0c43f0888eaa0e1
Anirudh Sylendranath <ani.22691@gmail.com>
2021-06-09 20:22:09 +0530

    Adding tailwind to project

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -33,7 +33,7 @@
 </template>
 
 <script lang="ts">
-  import { ref, defineComponent } from "vue";
+  import { defineComponent, ref } from "vue";
   export default defineComponent({
     name: "HelloWorld",
     props: {
